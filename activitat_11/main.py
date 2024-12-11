202b99492b500abdc405649f533a4eea1ff81a72
Albert <albertpencas@gmail.com>
2024-12-11 20:51:42 +0100

    He millorat l'imatge de la base de dades. Afegit fitxer conn, main afegir endpoints usuari i comencarPartida

--- activitat_11/main.py
+++ activitat_11/main.py
@@ -2,25 +2,37 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
 from pydantic import BaseModel
+from conn import *
 
-app = FastAPI
+
+app = FastAPI()
 
 # Base Model per les taules
 # Sol de la puntuació (de moment)
+@app.get("/")
+async def root():
+    return "Hello world"
 
-# Punt 2
-@app.get("/")
-def missatge():
-    return {
-        "Missatge" : "Començar partida" 
-            }
+
+# Punt de prova
+@app.get("/usuari")
+def mostrar_usuaris():
+    conn = db_conn()
+    cur = conn.cursor()
     
-# Punt 3
-@app.get("/")
-def missatge():
-    return {
-        "Missatge" : "Començar partida" 
-            }
+    cur.execute("SELECT * FROM USUARI")
+    resultat_consulta = cur.fetchall()
+    # con.commit -> no cal, insert
     
+    return resultat_consulta
+
+# Punt 2    
+@app.get("/comencarPartida")
+def mostrar_text_partida():
+    conn = db_conn()
+    cur = conn.cursor()
     
+    cur.execute("SELECT * FROM INFORMACIO")
+    resultat_consulta = cur.fetchall()
     
+    return resultat_consulta
